c9b44ee7e044d129654702838fb414b265142118
dsun03 <ds6760@nyu.edu>
2025-06-06 16:59:03 -0400

    debug messages

--- src/lib/supabaseServerClient.ts
+++ src/lib/supabaseServerClient.ts
@@ -14,13 +14,12 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+              console.log(`[COOKIE SET] ${name} = ${value}`, options);
+            });
+          } catch (err) {
+            console.warn('Could not set cookies in Server Component context.', err);
           }
         },
       },
